d01effc9d83f26a9eee0f716a1d464bcf37abd98
Peter Reutemann <fracpete@gmail.com>
2024-05-06 14:19:45 +1200

    requiring ldc>=0.2.3

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     },
     packages=find_namespace_packages(where='src'),
     install_requires=[
-        "llm-dataset-converter>=0.2.1",
+        "llm-dataset-converter>=0.2.3",
         "textract",
     ],
     version="0.0.1",
